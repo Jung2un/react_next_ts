bb3f0c202423a2e5049701d5ca684f74a3fe093e
Jung2un <a_su@daum.net>
2025-04-30 07:31:41 +0900

    feat: 모바일 화면 헤더 메뉴 개선

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -12,8 +12,18 @@ const Header: React.FC = () => {
 
     return (
         <header className={`${styles.header} ${scrollY > 50 ? styles.scrolled : ""}`}>
+            <div className={styles.navArea}>
+                <ul className={styles.headerNav}>
+                    <li><a href="#career">이력</a></li>
+                    <li><a href="#skills">기술</a></li>
+                    <li><a href="#projects">프로젝트</a></li>
+                    <li><a href="#portfolio">포트폴리오</a></li>
+                    <li><a href="#edu-cert">학력</a></li>
+                </ul>
+            </div>
+
             <button className={styles.themeToggle} onClick={toggleDarkMode}>
-                {isDarkMode ? <FaMoon size={20} /> : <FaSun size={20} />}
+                {isDarkMode ? <FaMoon size={20}/> : <FaSun size={20}/>}
             </button>
         </header>
     );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,13 +17,6 @@ export default function RootLayout({
 }) {
     return (
         <html lang="ko">
-        {/*<head>*/}
-        {/*    <link*/}
-        {/*        href="https://fonts.googleapis.com/css2?family=Diphylleia&display=swap"*/}
-        {/*        rel="stylesheet"*/}
-        {/*    />*/}
-        {/*    <meta name="viewport" content="width=device-width, initial-scale=1"/>*/}
-        {/*</head>*/}
         <body className={layout.pretend}>
         <DarkModeProvider>
             <Header />
